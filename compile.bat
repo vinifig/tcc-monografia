39a3791a92f41700a8728ff8b5ccde7144b0389e
vinifig <vinifig@hotmail.com>
2017-07-17 18:03:14 -0300

    monografia prototipo

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-xelatex monografia.tex -quiet && bibtex monografia.aux -quiet && xelatex monografia.tex && xelatex monografia.tex -quiet
+xelatex monografia.tex -quiet && bibtex referencias.bib -quiet && bibtex monografia.aux -quiet && xelatex monografia.tex && xelatex monografia.tex -quiet
